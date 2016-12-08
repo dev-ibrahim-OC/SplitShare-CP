8a05f77a1339a11fddd6e83abbca9c6b619a428b
ibrahim <fnu.ibrahim@eagles.oc.edu>
2016-12-08 11:04:20 +0000

    status 6th

--- mobile_app_dev/wsgi.py
+++ mobile_app_dev/wsgi.py
@@ -12,6 +12,5 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mobile_app_dev.settings")
-
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
