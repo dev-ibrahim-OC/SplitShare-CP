03f5541c506cc05eb79ddf170d94ff6c6b5b64f5
ibrahim <fnu.ibrahim@eagles.oc.edu>
2016-12-08 01:54:13 +0000

    commit error for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: gunicorn mobile_app_dev.wsgi --log-file -
--- mobile_app_dev/urls.py
+++ mobile_app_dev/urls.py
@@ -20,7 +20,7 @@ from snippets import views
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+     url(r'^admin/', admin.site.urls),
      url(r'^snippets/$', views.snippet_list),
      url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
