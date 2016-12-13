b17a34a69d20890f4444b04c75e37a50dd1c8503
ibrahim <fnu.ibrahim@eagles.oc.edu>
2016-12-13 05:36:24 +0000

    last commit

--- mobile_app_dev/urls.py
+++ mobile_app_dev/urls.py
@@ -21,7 +21,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<username>[a-z0-9]+)$', views.snippet_detail),
+    url(r'^snippets/(?P<username>[a-z0-9]/+)$', views.snippet_detail),
 ]
 
 
